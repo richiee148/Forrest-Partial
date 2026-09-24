84362ff501e194bcfb9c90fa3b179f0b2d3f3646
richiee148 <richellemaedealdo@gmail.com>
2026-09-24 20:05:14 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 
 import CustomerLogIn from './CustomerLogIn.jsx'
@@ -9,10 +9,12 @@ import CustomerSignUp from './CustomerSignUp.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
+     <Routes>
+        <Route path="/" element={<Navigate to="/CustomerSignUp" replace />} />
         <Route path="/CustomerSignUp" element={<CustomerSignUp />} />
         <Route path="/CustomerLogIn" element={<CustomerLogIn />} />
-      </Routes>
+        <Route path="*" element={<Navigate to="/CustomerSignUp" replace />} />
+    </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
